7707e60d8dff1aaeee5bdf68e586c6f342c7404b
AgusSchuller <agustin.schuller@gmail.com>
2022-11-08 22:33:14 -0300

    sprint 3

--- app.js
+++ app.js
@@ -1,26 +1,20 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const publicPath = path.resolve(__dirname, "./public");
 
-app.use(express.static(publicPath));
+
+const rutasMain = require ("./src/routes/main.js");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", rutasMain);
+
+//app.set("views", path.join(__dirname, "/src/views"));
+
+//app.set("view engine", "ejs");
+
+
 
 app.listen(3000, ()=>{
     console.log("Servidor funcionando");
-});
-
-app.get("/", (req, res)=>{
-    res.sendFile(__dirname + "/views/index.html");
-});
-app.get("/login", (req, res)=>{
-    res.sendFile(__dirname + "/views/login.html");
-});
-app.get("/productcart", (req, res)=>{
-    res.sendFile(__dirname + "/views/productCart.html");
-});
-app.get("/productdetail", (req, res)=>{
-    res.sendFile(__dirname + "/views/productDetail.html");
-});
-app.get("/register", (req, res)=>{
-    res.sendFile(__dirname + "/views/register.html");
 });
---
+++ src/controllers/mainController.js
@@ -0,0 +1,16 @@
+const path = require("path");
+
+const mainController = {
+    index: (req, res)=>{
+       // res.render("index")
+    //res.sendFile(path.resolve("./src/views/index.html"));
+}};
+
+module.exports = mainController;
+
+
+
+
+
+
+
