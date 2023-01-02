356ca6e2ee693e6bb982556e10db2ba468849e74
lolistover <lolistover@gmail.com>
2023-01-02 16:15:47 -0300

    Agrego validaciones

--- src/routes/users.js
+++ src/routes/users.js
@@ -28,11 +28,49 @@ var storage = multer.diskStorage({
 
 //********** VALIDACIONES ********** */
   const validacionesLogin = [
-
+    body('email').isEmail().withMessage('Agregar un email válido'),
+    body('password').isLength({min: 6 }).withMessage('La contraseña debe tener un mínimo de 6 caractéres'),
+    body('email').custom( (value ) =>{
+      for (let i = 0; i < archivoUsuarios.length; i++) {
+          if (archivoUsuarios[i].email == value) {
+              return true    
+          }
+      }
+      return false
+    }).withMessage('Usuario no regitrado'),
+    body('password').custom( (value, {req}) =>{
+        for (let i = 0; i < archivoUsuarios.length; i++) {
+            if (archivoUsuarios[i].email == req.body.email) {
+                if(bcrypt.compareSync(value, archivoUsuarios[i].password)){
+                  return true;
+                }else{
+                  return false;
+                }
+            }
+        }
+    }).withMessage('Usuario o contraseña no coinciden'),
   ]
 
   const validacionesRegistro = [
-
+    body('nombreApellido').isLength({
+      min: 1
+    }).withMessage('El campo Nombre y Apellido no puede estar vacío'),
+    body('email').isEmail().withMessage('Debe ingresar un email válido'),
+    body('password').isLength({min: 6 }).withMessage('La contraseña debe tener un mínimo de 6 caractéres'),
+    body('confirm_password').isLength({min: 6 }).withMessage('La confirmación de la contraseña debe tener un mínimo de 6 caractéres'),
+    body('confirm_password').custom((value, {req}) =>{
+      if(req.body.password == value ){
+          return true    
+      }else{
+          return false 
+      }    
+  }).withMessage('Las contraseñas deben ser iguales'),
+  body('image-users').custom((value, {req}) =>{
+    if(req.file != undefined){
+        return true
+    }
+    return false;
+}).withMessage('Debe seleccionar una imagen')
   ]
 
 /*** LOGIN ***/ 
@@ -41,7 +79,7 @@ router.post('/login', validacionesLogin,usersController.ingresar);
 
 /*** REGISTER ***/ 
 router.get("/register", usersController.register);
-router.post('/register', upload.single('image-users'),validacionesRegistro, usersController.crevate);
+router.post('/register', upload.single('image-users'),validacionesRegistro, usersController.create);
 
 /*** LOGOUT ***/ 
 router.get('/logout', usersController.logout);
