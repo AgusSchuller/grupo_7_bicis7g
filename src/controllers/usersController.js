573e835d6234bd64a9c0e9cac42ac63144f4597a
lolistover <lolistover@gmail.com>
2023-01-02 20:37:37 -0300

    Agrego mensajes de error en form registro

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -6,6 +6,8 @@ const users = JSON.parse(fs.readFileSync(usersFilePath, "utf-8"));
 
 const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
+const { validationResult } = require('express-validator');
+
 const usersController = {
   login: (req, res) => {
     res.render("./users/login");
@@ -13,14 +15,28 @@ const usersController = {
   register: (req, res) => {
     res.render("./users/register");
   },
-  ingresar: {
+  ingresar: (req, res) =>  {
     
   },
-  create: {
-    
-
+  processRegister:  (req, res) =>  {
+    let errors = validationResult(req);
+    if (errors.isEmpty()) {
+      let newUser = {
+				id: users[users.length - 1].id + 1,
+				nombre: req.body.nombre,
+				apellido: req.body.apellido,
+				email: req.body.email,
+				password: bcrypt.hashSync(req.body.password, salt),
+				imagen: req.file ? req.file.filename : "default-image.jpg",
+			};
+			users.push(newUser);
+			fs.writeFileSync(usersFilePath, JSON.stringify(users, null, " "));
+			res.redirect("/login");
+    }else{
+      return res.render("./users/register", { errors : errors.mapped(),old: req.body })
+    }
   },
-  logout: {
+  logout: (req, res) =>  {
     
 
   }
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,87 +1,117 @@
 // ************ Require's ************
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const path = require('path');
-const multer = require('multer');
-const bcrypt = require('bcryptjs');
-const fs = require('fs');
+const path = require("path");
+const multer = require("multer");
+const bcrypt = require("bcryptjs");
+const fs = require("fs");
 
 // ************ express-validator ************
-const { body } = require('express-validator');
+const { body } = require("express-validator");
 
 // ************ Controller Require ************
 const usersController = require("../controllers/usersController");
 
-let archivoUsuarios =  JSON.parse(fs.readFileSync(path.resolve(__dirname, '../database/users.json')))
+let archivoUsuarios = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, "../database/users.json"))
+);
 
 //********** MULTER ********** */
 var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, "./public/img/users");
-    },
-    filename: function (req, file, cb) {
-      cb(null, 'users-'+Date.now()+path.extname(file.originalname))
-    }
-  })
-   
-  const upload = multer({ storage })
+  destination: function (req, file, cb) {
+    cb(null, "./public/img/users");
+  },
+  filename: function (req, file, cb) {
+    cb(null, "users-" + Date.now() + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage });
 
 //********** VALIDACIONES ********** */
-  const validacionesLogin = [
-    body('email').isEmail().withMessage('Agregar un email válido'),
-    body('password').isLength({min: 6 }).withMessage('La contraseña debe tener un mínimo de 6 caractéres'),
-    body('email').custom( (value ) =>{
+const validacionesLogin = [
+  body("email").isEmail().withMessage("Agregar un email válido"),
+  body("password")
+    .isLength({ min: 6 })
+    .withMessage("La contraseña debe tener un mínimo de 6 caractéres"),
+  body("email")
+    .custom((value) => {
       for (let i = 0; i < archivoUsuarios.length; i++) {
-          if (archivoUsuarios[i].email == value) {
-              return true    
-          }
-      }
-      return false
-    }).withMessage('Usuario no regitrado'),
-    body('password').custom( (value, {req}) =>{
-        for (let i = 0; i < archivoUsuarios.length; i++) {
-            if (archivoUsuarios[i].email == req.body.email) {
-                if(bcrypt.compareSync(value, archivoUsuarios[i].password)){
-                  return true;
-                }else{
-                  return false;
-                }
-            }
+        if (archivoUsuarios[i].email == value) {
+          return true;
         }
-    }).withMessage('Usuario o contraseña no coinciden'),
-  ]
+      }
+      return false;
+    })
+    .withMessage("Usuario no regitrado"),
+  body("password")
+    .custom((value, { req }) => {
+      for (let i = 0; i < archivoUsuarios.length; i++) {
+        if (archivoUsuarios[i].email == req.body.email) {
+          if (bcrypt.compareSync(value, archivoUsuarios[i].password)) {
+            return true;
+          } else {
+            return false;
+          }
+        }
+      }
+    })
+    .withMessage("Usuario o contraseña no coinciden"),
+];
 
-  const validacionesRegistro = [
-    body('nombreApellido').isLength({
-      min: 1
-    }).withMessage('El campo Nombre y Apellido no puede estar vacío'),
-    body('email').isEmail().withMessage('Debe ingresar un email válido'),
-    body('password').isLength({min: 6 }).withMessage('La contraseña debe tener un mínimo de 6 caractéres'),
-    body('confirm_password').isLength({min: 6 }).withMessage('La confirmación de la contraseña debe tener un mínimo de 6 caractéres'),
-    body('confirm_password').custom((value, {req}) =>{
-      if(req.body.password == value ){
-          return true    
-      }else{
-          return false 
-      }    
-  }).withMessage('Las contraseñas deben ser iguales'),
-  body('image-users').custom((value, {req}) =>{
-    if(req.file != undefined){
-        return true
-    }
-    return false;
-}).withMessage('Debe seleccionar una imagen')
-  ]
+const validacionesRegistro = [
+  body("name")
+    .isLength({
+      min: 1,
+    })
+    .withMessage("El campo Nombre no puede estar vacío"),
+  body("apellido")
+    .isLength({
+      min: 1,
+    })
+    .withMessage("El campo Apellido no puede estar vacío"),
+  body("email").isEmail().withMessage("Debe ingresar un email válido"),
+  body("password")
+    .isLength({ min: 6 })
+    .withMessage("La contraseña debe tener un mínimo de 6 caractéres"),
+  body("confirm_password")
+    .isLength({ min: 6 })
+    .withMessage(
+      "La confirmación de la contraseña debe tener un mínimo de 6 caractéres"
+    ),
+  body("confirm_password")
+    .custom((value, { req }) => {
+      if (req.body.password == value) {
+        return true;
+      } else {
+        return false;
+      }
+    })
+    .withMessage("Las contraseñas deben ser iguales"),
+  body("image-users")
+    .custom((value, { req }) => {
+      if (req.file != undefined) {
+        return true;
+      }
+      return false;
+    })
+    .withMessage("Debe seleccionar una imagen"),
+];
 
-/*** LOGIN ***/ 
+/*** LOGIN ***/
 router.get("/login", usersController.login);
-router.post('/login', validacionesLogin,usersController.ingresar);
+router.post("/login", validacionesLogin, usersController.ingresar);
 
-/*** REGISTER ***/ 
+/*** REGISTER ***/
 router.get("/register", usersController.register);
-router.post('/register', upload.single('image-users'),validacionesRegistro, usersController.create);
+router.post(
+  "/register",
+  upload.single("image-users"),
+  validacionesRegistro,
+  usersController.processRegister
+);
 
-/*** LOGOUT ***/ 
-router.get('/logout', usersController.logout);
+/*** LOGOUT ***/
+router.get("/logout", usersController.logout);
 
 module.exports = router;
